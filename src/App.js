654fef9c4c66b8040cb65012c799f1429fae500f
Peter Kimani <peterjajakim@gmail.com>
2022-10-26 15:35:18 +0300

    CREATED SIGNUP PAGE

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-
+import SignUp from './components/SignUp';
 function App() {
   const navigate = useNavigate()
   return (
     <div className="App">
       <Routes>
         {/* add your routes here */}
-        <Route path="/mycomponent">
+        <Route path="/signup" element={ <SignUp/> }>
           {/* mycomponent goes here */}
         </Route>
-        <Route exact path="/" element={<button onClick={() => navigate("/mycomponent")}>Mycomponent</button>}/>
+        <Route exact path="/signup" element={<button onClick={() => navigate("/mycomponent")}>SIGN UP</button>}/>
       </Routes>
     </div>
   );
