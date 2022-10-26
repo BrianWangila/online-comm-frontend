5f8d2fe6202de84d6447a453dbfb7aa1e799fc4f
Brian Wangila <bwanjalah@gmail.com>
2022-10-27 00:39:42 +0300

    create single product component

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -16,32 +16,49 @@ function SingleProduct() {
     <Navbar />
       <div className='search-page'>
         <div className='flex-search'>
+          <div>
+            <h6>JUMIA</h6>
+          </div>
           <form className='search-input'>
-            <input type="search" value="Samsung A51" className='form-control'/>
+          {/* <i class="fa-thin fa-magnifying-glass"></i> */}
+            <input  type="search" className='form-control'></input>
           </form>
-
         </div>
 
-          <div className='results-card'>
+        <div className='display-image'>
+          <img src='https://thumbs.dreamstime.com/b/samsung-galaxy-s-plus-mobile-phone-taipei-taiwan-march-studio-shot-new-cellphone-reflective-white-surface-141609046.jpg' alt='phone'/>
+        </div>
+
+        <div className='similar-products'>
+          <div className='other-links'>
             <img src='https://thumbs.dreamstime.com/b/samsung-galaxy-s-plus-mobile-phone-taipei-taiwan-march-studio-shot-new-cellphone-reflective-white-surface-141609046.jpg' alt='phone'/>
-            <div className='vertical-line'></div>
-              <div className='super-content'>
-              <div className='content'>
-                <Typography gutterBottom variant="h6" component="div">
-                  Samsung Galaxy A51 | 128gb/4gb | Blue
+          </div>
+          <div className='other-links'>
+            <img src='https://thumbs.dreamstime.com/b/samsung-galaxy-s-plus-mobile-phone-taipei-taiwan-march-studio-shot-new-cellphone-reflective-white-surface-141609046.jpg' alt='phone'/>
+          </div>
+          <div className='other-links'>
+            <img src='https://thumbs.dreamstime.com/b/samsung-galaxy-s-plus-mobile-phone-taipei-taiwan-march-studio-shot-new-cellphone-reflective-white-surface-141609046.jpg' alt='phone'/>
+          </div>
+          
+        </div>
+
+            <div className=''>
+              <h6>Samsung Galaxy A51</h6>
+              <div className='product-details'>
+                <Typography gutterBottom variant="h7">
+                  Product: Samsung Galaxy A51 | 128gb/4gb | Blue
                 </Typography>
                 <Typography className='details'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation..."</Typography>
-                <Typography variant="body2" >Price: <span>Ksh.28,999/-</span></Typography>
-                <Typography variant="body2" color="text.secondary">Rating: <span>4.3 / 5.0</span></Typography>
-                <Typography variant="body2" color="text.secondary">Delivery: <span>250/-</span></Typography>
-                {/* <Typography variant="body2" color="text.secondary">Mode: <span>Pay after</span></Typography> */}
-                <h6 variant="body2">JUMIA</h6>
+                <Typography variant="body2">Price: <span>Ksh.28,999/-</span></Typography>
+                <Typography variant="body2">Rating: <span>4.3 / 5.0</span></Typography>
+                <Typography variant="body2">Delivery: <span>250/-</span></Typography>
+                <Typography variant="body2">Mode: <span>Pay after</span></Typography>
+
               </div>
               <div className='visit-shop'>
                 <Button style={{color: "whitesmoke"}} size="medium">VISIT SHOP</Button>
               </div>
             </div>
-          </div>
       </div>
       <Footer />
     </>
