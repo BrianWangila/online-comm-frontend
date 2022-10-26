a2c66a2c9a6a4e3010edcd0f79d4499cbe35e1be
Brian Wangila <bwanjalah@gmail.com>
2022-10-26 17:14:26 +0300

    add results card and style

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,12 +1,82 @@
 import React from 'react'
+import '../style/SearchResults.css'
+import { TextField, Button, Card, CardContent, Typography } from '@material-ui/core'
+
 
 
 
 function SearchResults() {
 
-  
+
   return (
-    <div>SearchResults</div>
+    <>
+      <div className='search-page'>
+        <div className='flex-search'>
+          <div className='search-input'>
+            <TextField id="outlined-basic" label="Product searched" variant="outlined" />
+          </div>
+
+          <div className='orderby'>
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Price</option>
+              <option value="1">Product</option>
+              <option value="2">Ratings</option>
+            </select>
+          </div>
+        </div>
+
+        <div className='row'>
+          <Card className='results-card' style={{borderRadius: 15+"px"}} >
+            <img src='https://thumbs.dreamstime.com/b/samsung-galaxy-s-plus-mobile-phone-taipei-taiwan-march-studio-shot-new-cellphone-reflective-white-surface-141609046.jpg' alt='phone'/>
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Samsung Galaxy A51 | 128gb/4gb | Blue
+              </Typography>
+              <Typography variant="body2" style={{color: "red"}}>Ksh.28,999/-</Typography>
+              <Typography variant="body2" color="text.secondary">Rating: <span>4.3 / 5.0</span></Typography>
+              <Typography variant="body2" color="text.secondary">Delivery: <span>250/-</span></Typography>
+              <h6 variant="body2" color="text.secondary">JUMIA</h6>
+            </CardContent>
+            <center className='visit-shop'>
+              <Button style={{color: "whitesmoke"}} size="small">VISIT SHOP</Button>
+            </center>
+          </Card>
+
+          <Card className='results-card' style={{borderRadius: 15+"px"}}>
+            <img src='https://thumbs.dreamstime.com/b/samsung-galaxy-s-plus-mobile-phone-taipei-taiwan-march-studio-shot-new-cellphone-reflective-white-surface-141609046.jpg' alt='phone'/>
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Samsung Galaxy A51 | 128gb/4gb | Blue
+              </Typography>
+              <Typography variant="body2" style={{color: "red"}}>Ksh.28,999/-</Typography>
+              <Typography variant="body2" color="text.secondary">Rating: <span>4.3 / 5.0</span></Typography>
+              <Typography variant="body2" color="text.secondary">Delivery: <span>250/-</span></Typography>
+              <h6 variant="body2" color="text.secondary">JUMIA</h6>
+            </CardContent>
+            <center className='visit-shop'>
+              <Button size="small">VISIT SHOP</Button>
+            </center>
+          </Card>
+
+          <Card className='results-card' style={{borderRadius: 15+"px"}}>
+            <img src='https://thumbs.dreamstime.com/b/samsung-galaxy-s-plus-mobile-phone-taipei-taiwan-march-studio-shot-new-cellphone-reflective-white-surface-141609046.jpg' alt='phone'/>
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Samsung Galaxy A51 | 128gb/4gb | Blue
+              </Typography>
+              <Typography variant="body2" style={{color: "red"}}>Ksh.28,999/-</Typography>
+              <Typography variant="body2" color="text.secondary">Rating: <span>4.3 / 5.0</span></Typography>
+              <Typography variant="body2" color="text.secondary">Delivery: <span>250/-</span></Typography>
+              <h6 variant="body2" color="text.secondary">JUMIA</h6>
+            </CardContent>
+            <center className='visit-shop'>
+              <Button size="small">VISIT SHOP</Button>
+            </center>
+          </Card>
+        </div>
+
+      </div>
+    </>
   )
 }
 
