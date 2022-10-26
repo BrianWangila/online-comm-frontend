e7ff25c1e0fea70aab1b49bbfcc5e7c0e3f43d75
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-26 11:13:47 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* add your routes here */}
+        <Route path="/mycomponent">
+          {/* mycomponent goes here */}
+        </Route>
+        <Route exact path="/">
+          <button onClick={() => navigate("/mycomponent")}>Mycomponent</button>
+        </Route>
+      </Routes>
     </div>
   );
 }
