583a78dad0388eccc46279af1002024c0e32976a
Brian Wangila <bwanjalah@gmail.com>
2022-10-26 22:04:15 +0300

    edit footer and style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,37 @@ function Footer() {
       <footer className='footer'>
         <div className='footer-murife'>
           <h6>MURIFE</h6>
+          <div className='quick-links'>
+            <h7>Quick links</h7>
+            <h7>About us</h7>
+          </div>
         </div>
+        <div className='footer-details'>
+          <div className='para'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+          <div>
+            <ul>
+              <li>Terms & Conditions</li>
+              <li>Privacy policy</li>
+              <li>Categories</li>
+              <li>Our brands</li>
+              <li>Search history</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Contact us</li>
+              <li>Billing policy</li>
+              <li>Copyright policy</li>
+              <li>Our partners</li>
+              <li>FAQ</li>
+            </ul>
+          </div>
+        </div>
+
+        <hr/>
+
         <div className='footer-icons'>
           <ul>
             <li>Icon</li>
