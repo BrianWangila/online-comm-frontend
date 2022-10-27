13fa92aedf4fb8665ad282221fdc17b221b3d853
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-27 09:10:48 +0300

    create product card

---
+++ src/components/ProductCard.js
@@ -0,0 +1,15 @@
+import '../style/product-card.css'
+
+export default function ProductCard(){
+    return(
+        <div>
+            <div>
+                <img/>
+            </div>
+            <div>
+                <span>{"product name"}</span>
+                
+            </div>
+        </div>
+    )
+}
