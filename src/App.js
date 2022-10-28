62232716260be0a1b90f88713f9d20b8358b7798
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-28 14:51:05 +0300

    removed duplicate

--- src/App.js
+++ src/App.js
@@ -61,48 +61,6 @@ function App() {
   return (
     <div className="App">
       <Routes>
-
-        {/* add your routes here */}
-        <Route path="/signup" element={ <SignUp/> }> </Route>
-        <Route path="/resetpassword" element={ <ForgotPassword/> }>
-          
-          {/* mycomponent goes here */}
-        </Route>
-        <Route path="/login" element={ <Login/> }>
-        </Route>
-        
-        <Route path="/navbar" element={<Navbar/>}/>
-        <Route path="/search" element={<>
-        <Navbar/>
-        <SearchPage/>
-        </>}/>
-        <Route path='/search-results' element={<SearchResults />}></Route>
-        <Route path='/footer' element={<Footer />}></Route>
-        <Route path='/single-product' element={<SingleProduct />}></Route>
-        <Route path='/products' element={
-        <>
-        <Navbar/>
-        <SearchPage setProducts={setProducts}/>
-        {products?<Products products={products} setProducts={setProducts}/>:null}
-        <Footer/>
-        </>
-      }></Route>
-        <Route exact path="/" element={
-        <div style={{display:"flex",flexDirection:"column"}}>
-          <button onClick={() => navigate("/mycomponent")}>Mycomponent</button>
-          <button onClick={() => navigate("/navbar")}>Navbar</button>
-          <button onClick={() => navigate("/signup")}>SignUp</button>
-          <button onClick={() => navigate("/search")}>Search</button>
-          <button onClick={() => navigate("/login")}>Login</button>
-          <button onClick={() => navigate("/resetpassword")}>Forgot Password</button>
-          <button onClick={() => navigate("/search-results")}>Search Results</button>
-          <button onClick={() => navigate("/footer")}>Footer</button>
-          <button onClick={() => navigate("/single-product")}>Single Product</button>
-          <button onClick={() => navigate("/products")}>Products</button>
-        </div>
-          }
-          />
-
         <Route exact path='/' element={
           <>
             <Navbar user={user}/>
@@ -113,21 +71,6 @@ function App() {
         }></Route>
         <Route path="/login" element={<Login onLogin={onLogin}/>}/>
         <Route path='/signup' element={<SignUp onLogin={onLogin}/>}/>
-        {/* <Route exact path="/" element={
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <button onClick={() => navigate("/mycomponent")}>Mycomponent</button>
-            <button onClick={() => navigate("/navbar")}>Navbar</button>
-            <button onClick={() => navigate("/signup")}>SignUp</button>
-            <button onClick={() => navigate("/search")}>Search</button>
-            <button onClick={() => navigate("/login")}>Login</button>
-            <button onClick={() => navigate("/search-results")}>Search Results</button>
-            <button onClick={() => navigate("/footer")}>Footer</button>
-            <button onClick={() => navigate("/single-product")}>Single Product</button>
-            <button onClick={() => navigate("/products")}>Products</button>
-          </div>
-        }
-        /> */}
-
       </Routes>
     </div>
   );
