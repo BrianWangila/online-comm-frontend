12c07e4725e3ef6a2a0cac7b43e7fc860760529b
Brian Wangila <bwanjalah@gmail.com>
2022-10-31 19:40:36 +0300

    edit navbar and userAvatar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../style/navbar.css'
 import UserAvatar from './UserAvatar'
 
 export default function Navbar({user}){
+    
+
     const navigate=useNavigate()
     function handleLoginOnclick(){
         navigate("/login")
     }
+
     return (
         <div className="navbar">
             <div className="navbar-logo">Murife</div>
             {user?
-            <div>
+            <div className='user-avatar' >
                 <UserAvatar user={user}/>
             </div>
             :<div>
