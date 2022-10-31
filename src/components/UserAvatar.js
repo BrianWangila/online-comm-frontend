c9b5d1d6ae1102af694216d1ac864797e007f06e
Peter Kimani <peterjajakim@gmail.com>
2022-10-31 15:06:03 +0300

    CORRECTED AVATAR ERRORS

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -1,9 +1,7 @@
 import React, { useState  } from 'react'
 import '../style/UserAvatar.css'
-
-import { Avatar, Dialog } from '@material-ui/core'
+import { Avatar } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
-import Profile from './Profile'
 import { GoogleLogout } from 'react-google-login';
 function UserAvatar({ user }) {
   const navigate = useNavigate()
@@ -13,19 +11,13 @@ function UserAvatar({ user }) {
   const handleLogout = () => {
     localStorage.removeItem('jwt')
     window. location. reload() 
+    
   }
 
   
 
   const handleProfile = () => {
     navigate('/profile')
-    }
-
-  const handleOpen = () => {
-    setOpen(true)
-  }
-  const handleClose = () => {
-    setOpen(false)
   }
  
   const logOut = () => {
@@ -33,27 +25,24 @@ function UserAvatar({ user }) {
     navigate('/login')
   };
 
-  const userName = user.user.username ? (user.user.username):(user.user.name)
 
   return (
     <>
       <div>
-        <div type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        <div className="" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
           <Avatar />
         </div>
       </div>
       <div className="collapse avatar-card" id="collapseExample">
         <ul className="card card-body text-li">
-          <li style={{ color: "orangered" }}>{userName}</li>
-          <li type="button" onClick={handleOpen}>Profile</li>
+          <li style={{color: "orangered"}}>{user.user.username}</li>
+          <li type="button" onClick={handleProfile}>Profile</li>
           <button onClick={handleLogout}>Logout</button> 
         </ul>
         <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
       </div>
-      <Dialog open={open} onClose={handleClose}>
-        <Profile handleClose={handleClose}/>
-      </Dialog>
     </>
   )
 }
-export default UserAvatar;
+
+export default UserAvatar
