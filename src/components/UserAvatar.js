7db5ed613c91fe15f9f53ea28eedfd4b3d069d7f
Brian Wangila <bwanjalah@gmail.com>
2022-10-31 14:15:47 +0300

    edit avatar and style

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -1,41 +1,45 @@
 import React, { useState,  } from 'react'
 import '../style/UserAvatar.css'
-import { Avatar } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+import { Avatar, Dialog } from '@material-ui/core'
+import Profile from './Profile'
 
 
 
 function UserAvatar({ user }) {
-  const navigate = useNavigate()
+  const {open, setOpen} = useState(false)
 
   const handleLogout = () => {
     localStorage.removeItem('jwt')
     window. location. reload() 
-    
   }
 
-  const handleProfile = () => {
-    navigate('/profile')
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
   }
 
-  
+  const userName = user.user.username ? (user.user.username):(user.user.name)
 
   return (
     <>
       <div>
-        <div className="" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        <div type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
           <Avatar />
         </div>
       </div>
       <div className="collapse avatar-card" id="collapseExample">
         <ul className="card card-body text-li">
-          <li style={{color: "orangered"}}>{user.user.username}</li>
-          <li type="button" onClick={handleProfile}>Profile</li>
+          <li style={{ color: "orangered" }}>{userName}</li>
+          <li type="button" onClick={handleOpen}>Profile</li>
           <button onClick={handleLogout}>Logout</button> 
         </ul>
       </div>
+      <Dialog open={open} onClose={handleClose}>
+        <Profile handleClose={handleClose}/>
+      </Dialog>
     </>
   )
 }
-
-export default UserAvatar
+export default UserAvatar;
